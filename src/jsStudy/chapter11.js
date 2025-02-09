542b59a23e74d5ccace33078d1220b4bf7dd81cd
kohj <080821@naver.com>
2025-02-09 23:14:43 +0900

    정리

--- src/jsStudy/chapter11.js
+++ src/jsStudy/chapter11.js
@@ -1,8 +1,10 @@
 console.log(1);
 
 //비동기 함수(3초가 지나면, 콜백함수 실행)
-setTimeout(()=>{
-    console.log(2);
+//Web Apis에 setTimeout 타이머와, 콜백함수까지 같이 넘겨줌
+//동기와 비동기 파일 참고
+setTimeout(() => {
+	console.log(2);
 }, 3000);
 
 console.log(3);
