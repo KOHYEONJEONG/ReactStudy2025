a70426af2314edc2e01a627033fef3fa799e4c33
DESKTOP-275CC7F\EM_NB163 <hyeonjeong.ko@easymedia.net>
2025-02-17 16:58:11 +0900

    useReducer 공부

--- src/components/Exam.jsx
+++ src/components/Exam.jsx
@@ -7,6 +7,14 @@ function reducer(state, action) {
 	//첫번째 : 변환기 역할, 두번째 : 초기값
 	//action 객체가 매개변수로 전달받음.
 	console.log(state, action);
+	switch (action.type) {
+		case "INCREASE":
+			return (state += action.data);
+		case "DECREASE":
+			return (state -= action.data);
+		default:
+			return state;
+	}
 }
 function Exam() {
 	//dispatch : 발송하다, 급송하다.
@@ -15,16 +23,25 @@ function Exam() {
 
 	const onClickPlus = () => {
 		//인수 : 상태가 어떻게 변화되길 원하는지
-		// -> 왈 : 액션 객체(객체형태로 type, data)
+		// -> 왈 : ✅액션 객체(객체형태로 type, data)
 		dispatch({
 			type: "INCREASE", //증가
 			data: 1, //1만큼
 		});
 	};
+
+	const onClickMinus = () => {
+		dispatch({
+			type: "DECREASE", //감소소
+			data: 1, //1만큼
+		});
+	};
+
 	return (
 		<div>
 			<h1>{state}</h1>
 			<button onClick={onClickPlus}>+</button>
+			<button onClick={onClickMinus}>-</button>
 		</div>
 	);
 }
