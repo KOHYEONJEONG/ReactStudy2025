b9c82ff2cb2e2d4b78864da0b1a192f6b658b21e
KOHYEONJEONG <080821@naver.com>
2025-06-10 00:27:35 +0900

    훅 분리

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
+import { useState } from "react";
 import "./App.css";
 
+const useCount = () => {
+	const [count, setCount] = useState(0);
+
+	return [count, setCount];
+};
 function App() {
-    return (
-        <></>
-    );
+	const [count, setCount] = useCount();
+
+	return (
+		<div>
+			{count}
+			<button onClick={() => setCount((C) => C + 1)}>+1</button>
+		</div>
+	);
 }
 export default App;
